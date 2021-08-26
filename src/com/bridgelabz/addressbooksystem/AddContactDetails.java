7e44bffbb2061cf6904292c411d2d2464a15eebe
padmini v <padmini.v@perfios.com>
2021-08-27 00:52:00 +0530

    ADDED: Ability to edit the existing contact details

--- src/com/bridgelabz/addressbooksystem/AddContactDetails.java
+++ src/com/bridgelabz/addressbooksystem/AddContactDetails.java
@@ -5,36 +5,110 @@ import java.util.Scanner;
 
 public class AddContactDetails
 {
-	private ArrayList<EditDetails> addressBook = new ArrayList<EditDetails>();
-	public void addPerson() 
-	{
+	private ArrayList<EditDetails> addressList = new ArrayList<EditDetails>();
+
+	public void addPerson() {
+		System.out.println("Enter Person details:");
+		addressList.add(addContact());
+	}
+	public static EditDetails addContact() {
 		Scanner sc = new Scanner(System.in);
 		EditDetails contact = new EditDetails();
-		System.out.println("-------Enter the Contact Details--------");
-		System.out.print("Enter First Name: ");
-		sc.next();
-		contact.setFirstName(sc.nextLine());
+
+		System.out.print("Enter First Name:");
+		contact.setFirstName(sc.next());
 		System.out.print("Enter Second Name: ");
-		sc.next();
-		contact.setLastName(sc.nextLine());
+		contact.setLastName(sc.next());
 		System.out.print("Enter Address: ");
-		sc.next();
-		contact.setAddress(sc.nextLine());
+		contact.setAddress(sc.next());
 		System.out.print("Enter City: ");
 		contact.setCity(sc.next());
 		System.out.print("Enter State: ");
-		sc.next();
-		contact.setState(sc.nextLine());
+		contact.setState(sc.next());
 		System.out.print("Enter Pin code: ");
-		contact.setPinCode(sc.next());
+		contact.setPinCode(sc.nextLine());
 		System.out.print("Enter Phone nmber: ");
 		contact.setPhoneNumber(sc.next());
-		System.out.print("Enter email: ");
+		System.out.print("Enter Email: ");
 		contact.setEmail(sc.next());
-		sc.close();
-		addressBook.add(contact);
+		return contact;
+	}
+
+	public void editPerson(String name) {
+		EditDetails contact = null ;
+		int i = 0;
+		if(addressList.size()!=0) {
+			while(i < addressList.size()) {
+				if(addressList.get(i).getFirstName().equals(name)) {
+					contact = addressList.get(i);
+					break;
+				}
+				i++;
+			}
+			if(contact == null) {
+				System.out.println("Name not found");
+				return;
+			}
+
+			System.out.println("Enter new details  of "+name);
+			addressList.set(i,addContact());
+			System.out.println("Modified Details Successfully");
+		}
+		else
+		{
+			System.out.println("Add some contacts to Edit");
+		}
+	}
+
+	public void display() {
+		Scanner sc = new Scanner(System.in);
+		EditDetails contact = null; 
+		if(addressList.size()!=0) 
+		{
+		for(int i=0; i<addressList.size();i++) 
+		{
+			System.out.print(addressList.get(i).getFirstName()+"  ");
+		}
+		System.out.println();
+		System.out.println("Enter name to see details");
+		String name = sc.next();
+
+		for(int i = 0;i < addressList.size();i++) 
+		{
+			if(addressList.get(i).getFirstName().equals(name)) 
+			{
+				contact = addressList.get(i);
+				break;
+			}
+		}
+		if(contact == null) 
+		{
+			System.out.println("name not found");
+			return;
+		}
+
+		displayEditDetails(contact);
+		}
+		else
+		{
+			System.out.println("Add contacts");
+		}
+		
 
 	}
 
 
+
+	public static void displayEditDetails(EditDetails contact) {
+		System.out.println("First Name : "+contact.getFirstName());
+		System.out.println("Second Name : "+ contact.getLastName());
+		System.out.println("Address : "+ contact.getAddress());
+		System.out.println("City : "+contact.getCity());
+		System.out.println("State : "+contact.getState());
+		System.out.println("Pin code : "+contact.getPinCode());
+		System.out.println("Phone nmber : "+contact.getPhoneNumber() );
+		System.out.println("Email : "+contact.getEmail());
+	}
 }
+
+
--- src/com/bridgelabz/addressbooksystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -1,10 +1,33 @@
 package com.bridgelabz.addressbooksystem;
 
+import java.util.Scanner;
 public class AddressBookMain {
 	public static void main(String args[]) {
-		System.out.println("**********  Welcome to Address Book Management Program  ************");
+		System.out.println("******  Welcome to Address Book Management Program  ********");
 
+		Scanner sc = new Scanner(System.in);
 		AddContactDetails addressBook = new AddContactDetails();
-		addressBook.addPerson();
+		int choice = 0;
+		boolean exit = true;
+		while(exit) {
+			System.out.println("Enter 1.Add Contact 2.Edit existing Contact 3.Display 4.Exit");
+			choice  = sc.nextInt();
+			switch(choice) {
+			case 1 :
+				addressBook.addPerson();
+				break;
+			case 2 :
+				System.out.println("Enter Contact's Name to Edit Details");
+				addressBook.editPerson(sc.next());
+				break;
+			case 3:
+				addressBook.display();
+				break;
+			default:
+				exit = false;
+
+			}
+			System.out.println();
+		}
 	}
 }
