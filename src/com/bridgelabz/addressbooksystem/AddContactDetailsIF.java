5de5de927ff01f5bf4f07e49aa6aa7668ed742d3
padmini v <padmini.v@perfios.com>
2021-09-02 12:14:05 +0530

    ADDED: AddContactDetailsIF interface to perform operations on addressBook

---
+++ src/com/bridgelabz/addressbooksystem/AddContactDetailsIF.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.addressbooksystem;
+
+public interface AddContactDetailsIF 
+{
+
+		public void addContact();
+		
+		public void showContacts();
+		
+		public void editContact();
+		
+		public void deleteContact();
+
+}
