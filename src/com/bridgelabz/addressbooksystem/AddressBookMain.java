5800e413a0de30649435c29146798de31e32ddb8
padmini v <padmini.v@perfios.com>
2021-09-12 19:49:21 +0530

    REFACTORED: Access to methods of MultipleAddressBook class in menu method

--- src/com/bridgelabz/addressbooksystem/AddressBookMain.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -3,42 +3,45 @@ package com.bridgelabz.addressbooksystem;
 import java.util.Scanner;
 public class AddressBookMain 
 {
+
 	public static void main(String[] args) 
 	{
-		System.out.println("*****Welcome to Address Book Program*****");
-		MultipleAddressBook addressBooks = new MultipleAddressBook();
-		menu(addressBooks);
+		System.out.println("***Welcome to Address Book System***");
+		MultipleAddressBook multipleAddressBook = new MultipleAddressBook();
+		menu(multipleAddressBook);
 	}
 
-	public static void menu(MultipleAddressBook addressBooks) 
+	public static void menu(MultipleAddressBook multipleAddressBook) 
 	{
+		AddContactDetails contactDetails = new AddContactDetails();
+		String addressBookName = "";
 		Scanner scanner = new Scanner(System.in);
 		for (;;) 
 		{
 			for (;;) 
 			{
-				System.out.println("1.create new address book\n2.edit existing address book\n3.show address books\n4.exit");
+				System.out.println("1.create new address book  2.edit existing address book  3.show address books 4.exit");
 				int choice = scanner.nextInt();
 				if (choice == 1) 
 				{
-					addressBooks.addAddressBooks();
+					multipleAddressBook.addAddressBooks();
 				} 
 				else if (choice == 2) 
 				{
-					if (addressBooks.selectAddressBook() == 1)
+					System.out.println("enter address book name");
+					addressBookName = scanner.next();
+					if (multipleAddressBook.selectAddressBook(addressBookName) != null)
 						break;
 				} 
 				else if (choice == 3) 
 				{
-					addressBooks.showAddressBook();
+					multipleAddressBook.showAddressBook();
 				} 
 				else 
-				{
 					return;
-				}
 			}
 
-			System.out.println("1.Add contact\n2.Show contact\n3.Edit contact\n4.Delete contact\n5.exit");
+			System.out.println("1.add contact 2.show contact 3.edit contact 4.delete contact 5.exit");
 			int choice = scanner.nextInt();
 			if (choice == 5)
 				break;
@@ -46,16 +49,16 @@ public class AddressBookMain
 			switch (choice) 
 			{
 			case 1:
-				addressBooks.addContact();
+				contactDetails.addContact(multipleAddressBook.selectAddressBook(addressBookName).addressBook);
 				break;
 			case 2:
-				addressBooks.showContacts();
+				contactDetails.showContacts(multipleAddressBook.selectAddressBook(addressBookName).addressBook);
 				break;
 			case 3:
-				addressBooks.editContact();
+				contactDetails.editContact(multipleAddressBook.selectAddressBook(addressBookName).addressBook);
 				break;
 			case 4:
-				addressBooks.deleteContact();
+				contactDetails.deleteContact(multipleAddressBook.selectAddressBook(addressBookName).addressBook);
 				break;
 			}
 
