66d2f544688e4d043d352d0da404ad7785e6278a
padmini v <padmini.v@perfios.com>
2021-09-19 23:37:04 +0530

    REFACTORED: added methods to read and write the address book data to file

--- src/com/bridgelabz/addressbooksystem/AddContactDetailsIF.java
+++ src/com/bridgelabz/addressbooksystem/AddContactDetailsIF.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.addressbooksystem;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public interface AddContactDetailsIF 
 {
@@ -12,5 +14,19 @@ public interface AddContactDetailsIF
 		public void editContact(HashMap<String, EditDetails> addressBookContact);
 		
 		public void deleteContact(HashMap<String, EditDetails> addressBookContact);
+		
+		public void personWithCity(EditDetails contact);
+		
+		public void personWithState(EditDetails contact);
+		
+		public void showPersonList(String location, HashMap<String, ArrayList<EditDetails>> contactsList);
+		
+		public void showCountofContacts(String location, HashMap<String, ArrayList<EditDetails>> contactsList);
+		
+		public void sortContacts(HashMap<String, EditDetails> addressBook, int sortKey);
+		
+		public void writeToAddressBookFile();
+
+		public List<String> readAddressDataFromFile();
 
 }
